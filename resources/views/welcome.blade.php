f3f87c6dee63dc78e105d271fd66f6265fab52cb
Lana <44567161+lanasvet12@users.noreply.github.com>
2020-03-19 17:40:02 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hello Heroku World!
+                    Its great Laravel World!
                 </div>
 
                 <div class="links">
